cf0c50192cbe76a81d2151c7f5e9d510b00090c6
Adrián <adrian.lopezma@gmail.com>
2019-09-02 16:16:44 +0200

    finished chapter 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,8 @@ import { PaCellColorSwitcher } from './product/cellColorSwitcher.directive';
 import { ProductTableComponent } from './productTable.component';
 import { ProductFormComponent } from './productForm.component';
 import { PaToggleView } from './toggleView.component';
+import { PaAddTaxPipe } from './addTax.pipe';
+import { PaCategoryFilterPipe } from './categoryFilter.pipe';
 
 @NgModule({
   declarations: [
@@ -25,7 +27,9 @@ import { PaToggleView } from './toggleView.component';
     PaCellColorSwitcher,
     ProductTableComponent,
     ProductFormComponent,
-    PaToggleView
+    PaToggleView,
+    PaAddTaxPipe,
+    PaCategoryFilterPipe
   ],
   imports: [
     BrowserModule,
--- src/app/productTable.component.ts
+++ src/app/productTable.component.ts
@@ -1,15 +1,13 @@
 import { Component, Input, ViewChildren, QueryList } from "@angular/core";
 import { Model } from './product/repository.model';
 import { Product } from './product/product.model';
-import { PaCellColor } from './product/cellColor.directive';
-
 @Component({
-    selector: 'paProductTable',
-    templateUrl: './productTable.component.html'
+    selector: "paProductTable",
+    templateUrl: "productTable.component.html"
 })
 export class ProductTableComponent {
-    @Input('model') dataModel: Model;
-
+    @Input("model")
+    dataModel: Model;
     getProduct(key: number): Product {
         return this.dataModel.getProduct(key);
     }
@@ -18,22 +16,5 @@ export class ProductTableComponent {
     }
     deleteProduct(key: number) {
         this.dataModel.deleteProduct(key);
-    }
-    showTable: boolean = true;
-
-    @ViewChildren(PaCellColor) viewChildren: QueryList<PaCellColor>;
-
-    ngAfterViewInit() {
-        this.viewChildren.changes.subscribe(() => {
-            this.updateViewChildren();
-        });
-        this.updateViewChildren();
-    }
-    private updateViewChildren() {
-        setTimeout(() => {
-            this.viewChildren.forEach((child, index) => {
-                child.setColor(index % 2 ? true : false);
-            })
-        }, 0);
     }
 }
